0b4019adb1d62d763252432a3ca7123ee284bf23
Devran Uenal <maccosmo@gmail.com>
2012-10-21 04:27:34 +0200

    don't ask :D

--- app/assets/javascripts/ui.js
+++ app/assets/javascripts/ui.js
@@ -1,32 +1,32 @@
 $(document).ready(function(){
-	$("#video-trigger-minified-open").click(function() {
+	$("#video-trigger-minified").click(function() {
 		if($("#video").css("margin-top") === "-323px"){
 			$("#video").animate({"margin-top" : "0"}, 300, 'swing');
-			$("#current-video").slideUp(400);
-			$("#video-trigger-minified-open").css({"background-color" : "#ff4300"});
-			$("#info-trigger-minified-open").css({"background-color" : "#222"});
+			$("#infocard").slideUp(400);
+			$("#video-trigger-minified").css({"background-color" : "#ff4300"});
+			$("#info-trigger-minified").css({"background-color" : "#222"});
 		} else {
 			$("#video").animate({"margin-top" : "-323px"}, 300, 'swing');
-			$("#video-trigger-minified-open").css({"background-color" : "#222"});
+			$("#video-trigger-minified").css({"background-color" : "#222"});
 		}
 	});
 
 	$("#video-trigger").click(function() {
 		$("#video").animate({"margin-top" : "0"}, 300, 'swing');
-		$("#current-video").slideUp(400);
-		$("#video-trigger-minified-open").css({"background-color" : "#ff4300"});
-		$("#info-trigger-minified-open").css({"background-color" : "#222"});
+		$("#infocard").slideUp(400);
+		$("#video-trigger-minified").css({"background-color" : "#ff4300"});
+		$("#info-trigger-minified").css({"background-color" : "#222"});
 	});
 
-	$("#info-trigger-minified-open").click(function() {
-		if($("#current-video").is(":hidden")){
-			$("#current-video").slideDown('fast');
+	$("#info-trigger-minified").click(function() {
+		if($("#infocard").is(":hidden")){
+			$("#infocard").slideDown('fast');
 			$("#video").animate({"margin-top" : "-323px"}, 300, 'swing');
-			$("#video-trigger-minified-open").css({"background-color" : "#222"});
-			$("#info-trigger-minified-open").css({"background-color" : "#ff4300"});
+			$("#video-trigger-minified").css({"background-color" : "#222"});
+			$("#info-trigger-minified").css({"background-color" : "#ff4300"});
 		} else {
-			$("#current-video").slideUp('fast');
-			$("#info-trigger-minified-open").css({"background-color" : "#222"});
+			$("#infocard").slideUp('fast');
+			$("#info-trigger-minified").css({"background-color" : "#222"});
 		}
 	});
 });
